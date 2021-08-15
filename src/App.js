b6e5dd42e63e2dbdf8dcb73761c6500fa5f894b8
MeghaDhotarkar <88981544+MeghaDhotarkar@users.noreply.github.com>
2021-08-15 22:35:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React ,{useState,useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+import Header from './components/Header'
+import CharacterList from './components/CharacterList';
+import Pagination from './components/Pagination';
+
+const NoMatchRoute = () => <div>404 Page</div>;
+const App = ()=> {
+
+  const [items,setItems] = useState([]);
+  const [isLoading,setIsLoading] = useState(true);
+  const [currentPage,setCurrentPage] = useState(1);
+  const [itemPerPage,setItemPerPage] = useState(10);
+  const [item,setItem] = useState([]);
+
+  useEffect(() => {
+    const fetchItem = async () => {
+     const result = await axios('https://www.breakingbadapi.com/api/characters')
+
+     console.log(result.data)
+
+     setItems(result.data)
+     setIsLoading(false)
+    }
+    fetchItem()
+  }, [])
+
+  const indexOfLastItem = currentPage * itemPerPage;
+  const indexOfFirstItem = indexOfLastItem-itemPerPage;
+  const currentItem = items.slice(indexOfFirstItem,indexOfLastItem);
+
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  return (
+    <div className="container">
+      <Header/>
+      <CharacterList isLoading={isLoading} items={currentItem} />
+      
+      <Pagination 
+        itemPerPage={itemPerPage}
+        totalItems={items.length}
+        paginate={paginate}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CharacterList.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const CharacterList = ({items,isLoading}) => {
+    return (
+        isLoading?<h1>Lading..</h1>:<section className='list-group mb-4'>
+            <table> 
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>DateOfBirth</th>
+                        <th>Status</th>
+                        <th>Occupation</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                 {items.map(item => {
+                   
+                     return (
+                      
+                       <tr>
+                       
+                           <td >{item.char_id} </td>
+                           <td>{item.name}</td>
+                           <td>{item.birthday}</td>
+                           <td>{item.status}</td>
+                           <td>{item.occupation}<button onClick={()=> window.open().
+                            document.write('<div className="container">'+item.name+"<br />"+
+                                         '<img src='+item.img+'><br />'+
+                                         item.birthday+"<br />"+
+                                         item.nickname+"<br />"+
+                                         item.occupation+"<br />"+
+                                         item.portrayed+"<br />"+
+                                            item.status+'</div>')}>Detail</button></td>
+                           
+                       </tr>
+                                            
+                     );
+                 })}
+                </tbody>
+            </table>
+            
+        </section>
+    )
+}
+
+export default CharacterList
